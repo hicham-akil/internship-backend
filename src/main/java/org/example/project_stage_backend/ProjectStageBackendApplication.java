62dba6e2adb6ae77fcb3da983458f14f6918ad04
Hicham Akil <hichamakil2018@gmail.com>
2026-04-20 19:29:20 +0100

    first commit in main branche

---
+++ src/main/java/org/example/project_stage_backend/ProjectStageBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.project_stage_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectStageBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectStageBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/project_stage_backend/ProjectStageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.project_stage_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectStageBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
