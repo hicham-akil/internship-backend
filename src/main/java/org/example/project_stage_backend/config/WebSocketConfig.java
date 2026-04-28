07c7289e9fa9d34904255edf6dac1a6b05396cfb
Hicham Akil <hichamakil2018@gmail.com>
2026-04-28 20:32:29 +0100

    fixing routes paths and websocketconfig

--- src/main/java/org/example/project_stage_backend/config/WebSocketConfig.java
+++ src/main/java/org/example/project_stage_backend/config/WebSocketConfig.java
@@ -16,7 +16,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-jfc1")             // URL de connexion desktop
-                .setAllowedOriginPatterns("*");       // restreindre en prod
+        registry.addEndpoint("/ws-jfc1")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();  // ← AJOUTER
     }
 }
--- src/main/java/org/example/project_stage_backend/controller/IndicateurController.java
+++ src/main/java/org/example/project_stage_backend/controller/IndicateurController.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/indicateurs")
+@RequestMapping("/indicateurs")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 public class IndicateurController {
